c8115f41658fba267f34b3cbfc476ec5c58d6acb
Fernanda Palacios <fernandapalacios.dev@gmail.com>
2022-09-21 21:03:38 -0500

    functions

---
+++ 01-intro-node/02-first-scripts.js
@@ -0,0 +1,82 @@
+/*
+Crear una funcion que reciba un nombre como parametro y devuelva un saludo con dicho nombre
+
+Output -> 'Hola {alguien}, buenas noches'
+
+*/
+
+function greeting(name) {
+    return `Hello ${name}, buenas noches` // string
+}
+
+const greetFer = greeting('Fers')
+
+console.log(greetFer) // string
+
+const nameXavy = 'Xavy'
+
+const greetXavy = greeting(nameXavy)
+
+console.log(greetXavy)
+
+/*
+Crear una función que me devuelva un nombre aleatorio de un arreglo
+*/
+
+// declarando/definiendo una funcion
+function getNameRandom(){
+    const arrayNames = ['Benja', 'Cin', 'Rafa', 'Jona', 'Xavy', 'Annie']
+    const min = 0
+    const max = arrayNames.length
+    const indexRandom = Math.floor(Math.random() * (max - min) + min)
+    return arrayNames[indexRandom]
+}
+
+// Ejecución de la función
+getNameRandom()
+
+console.log('Obteniendo un nombre random')
+
+
+
+                    // Pasando como argumento la ejecución de una función
+const firstGreet = greeting(getNameRandom())
+
+const nameRandom = getNameRandom()
+const secondGreet = greeting(nameRandom)
+
+console.log(firstGreet)
+console.log(secondGreet)
+
+// 
+
+function hola(name) {
+    return `Hola, ${name}, buenas noches`
+}
+
+
+const x = hola('Xavy') //
+console.log('x', x)
+
+// Pasar como referencia
+const otraFuncion = hola
+console.log(otraFuncion)
+const a = otraFuncion
+const b =  hola
+
+console.log(b('Rodri'))
+
+// 
+
+const product = () => {}
+
+// Funciones autoejecutables
+
+// factory function -> funciones que general funciones
+
+
+
+
+
+
+
---
+++ 01-intro-node/hello-node.js
@@ -0,0 +1,28 @@
+
+console.log('Hola Node :D')
+
+let myName = "Fernanda Palacios"
+const HOST = 'http://localhost:8080'
+
+function sum(num1, num2) {
+    return num1 + num2
+}
+
+sum(10,12)
+
+const resta = (num1, num2) => num1 - num2
+
+if(false){
+    console.log('Se ejecuta cuando es verdaderoº')
+}else {
+    console.log('Se ejecuta cuando es falso')
+}
+
+// Browser - no podremos utilizarlo
+// Manipulacion del DOM
+// window
+// fetch('')
+
+// prompt('')
+// alert('Soy un alert')
+// confirm('¿Estas seguro de borrar esto?')
